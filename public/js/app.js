69038ee8744a4a410c31eb1b9d983f6376466d04
George Liu <georgeliu@Georges-MacBook-Pro.local>
2022-10-05 23:42:50 +1100

    Removed unnecessary

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('This is the app.js file')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
